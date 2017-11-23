774e0e3646bba583574b23c96f69cd67b99920c2
stanislav42 <StadnikStanislav42@gmail.com>
2017-11-23 16:41:28 +0300

    .

--- fish/ajax.php
+++ fish/ajax.php
@@ -8,11 +8,10 @@ if ($_POST['call']=='1'){
   $mess = "Заказать звонок. Свяжитесь пожалуйста с ".$_POST['name']." по этому номеру ".$_POST['tel'].", а также email'у: ".$_POST['myemail']."Клиент пришел от ".$_POST['source'];
 }
 else{
-  $mess = "Прайс. Свяжитесь пожалуйста с ".$_POST['name']." по этому номеру ".$_POST['tel']. ". Емайл: ".$_POST['myemail'].". Область - ".$_POST['area'].". Клиент пришел от ".$_POST['source'];
+  $mess = "Прайс. Свяжитесь пожалуйста с ".$_POST['name']." по этому номеру ".$_POST['tel']. ". Емайл: ".$_POST['myemail'].". Область - ".$_POST['area'].". Клиент пришел от ".$_POST['source']. " Клиент выбрал вид рыбы :".$_POST['fishname'];
 }
 //$mess = iconv("UTF-8", "ISO-8859-1", $mess);
 //mail("petrotsymb@gmail.com", "Сообщение с сайта Золотой резерв", $mess, $header);
-mail("4@kaeros.ru", "Сообщение с сайта Золотой резерв", $mess, $header);
-mail_1("stadnikstanislav42@gmail.com", "Сообщение с сайта Золотой резерв килька", $mess, $header);
+mail("stadnikstanislav42@gmail.com", "Сообщение с сайта Золотой резерв", $mess, $header);
 /*mail("kvmamontov@gmail.com", "Сообщение с сайта Золотой резерв", $mess, $header);*/
 ?>
