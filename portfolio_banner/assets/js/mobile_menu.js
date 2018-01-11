c63a57d9251921047aa2a4213ad9f21b6e7071a5
stanislav42 <StadnikStanislav42@gmail.com>
2018-01-11 20:08:58 +0300

    .

---
+++ portfolio_banner/assets/js/mobile_menu.js
@@ -0,0 +1,12 @@
+$('.burger_deluxe').on('click',function() {
+
+  $(this).toggleClass('open');
+  $('.overlay').toggleClass('open');
+  
+});
+
+$('.link__wrapper-item').on('click',function() {
+
+  $('.burger_deluxe').toggleClass('open');
+  $('.overlay').toggleClass('open');
+});
